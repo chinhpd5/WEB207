514c609eb87fbd502ae461bcf7c7394ae785345c
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-18 17:47:42 +0700

    checkbox

--- lession8/main.js
+++ lession8/main.js
@@ -1,7 +1,25 @@
 const app = angular.module("myApp",[]);
 
 function myFunction($scope){
-    $scope.dataInput={};
+    $scope.dataInput={
+        hobbies : [
+            {
+                text: "Đá bóng",
+                name: "football",
+                value: false
+            },
+            {
+                text: "Bơi",
+                name: "swim",
+                value: false
+            },
+            {
+                text: "Chạy",
+                name: "run",
+                value: false
+            }
+        ]
+    };
     $scope.listGender =[
         {
             name: "Nam",

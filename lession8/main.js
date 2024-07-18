7efad300abdeb782a056c72fb66102b7ad6b0481
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-18 20:06:35 +0700

    select

--- lession8/main.js
+++ lession8/main.js
@@ -1,6 +1,32 @@
 const app = angular.module("myApp",[]);
 
 function myFunction($scope){
+    $scope.listData=[
+        {
+            gender: "other",
+            hobbies: [
+                {
+                    name: "football",
+                    text: "Đá bóng",
+                    value: true
+                },
+                {
+                    name: "swim",
+                    text: "Bơi",
+                    value: true
+                },
+                {
+                    name: "run",
+                    text: "Chạy",
+                    value: false
+                }
+            ],
+            subject: "WEB207",
+            name: "chinhpd5",
+            age: 20
+        }
+    ]
+    
     $scope.listGender= [
         {
             name: "Nam",
@@ -13,6 +39,21 @@ function myFunction($scope){
         {
             name: "Khác",
             value: "other"
+        }
+    ]
+
+    $scope.listSubject =[
+        {
+            id: 'WEB207',
+            name: 'Frontend Framework'
+        },
+        {
+            id: 'WEB208',
+            name: 'PHP 2'
+        },
+        {
+            id: 'WEB209',
+            name: 'PHP 3'
         }
     ]
 
@@ -34,13 +75,16 @@ function myFunction($scope){
                 text: "Chạy",
                 value: false
             }
-        ]
+        ],
+        subject: $scope.listSubject[0].id
     }
 
     
 
     $scope.onSubmit = function(){
-        console.log($scope.dataInput);
+        // console.log($scope.dataInput);
+        $scope.listData.push(angular.copy($scope.dataInput))
+        console.log($scope.listData);
     }
 
 

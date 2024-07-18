3467dfff59dfdf85c2536f50c7920dcbe9f8d6fd
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-18 19:27:24 +0700

    radio repeat

--- lession8/main.js
+++ lession8/main.js
@@ -1,11 +1,32 @@
 const app = angular.module("myApp",[]);
 
 function myFunction($scope){
-    $scope.dataInput={}
+    $scope.listGender= [
+        {
+            name: "Nam",
+            value: "men"
+        },
+        {
+            name: "Nữ",
+            value: "women"
+        },
+        {
+            name: "Khác",
+            value: "other"
+        }
+    ]
+
+    $scope.dataInput={
+        gender : $scope.listGender[$scope.listGender.length - 1].value
+    }
+
+    
 
     $scope.onSubmit = function(){
         console.log($scope.dataInput);
     }
+
+
 
 }
 

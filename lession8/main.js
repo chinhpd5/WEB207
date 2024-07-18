e9c9c951f1c55b629f98293884ebfc68210c4491
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-18 19:02:20 +0700

    form

---
+++ lession8/main.js
@@ -0,0 +1,12 @@
+const app = angular.module("myApp",[]);
+
+function myFunction($scope){
+    $scope.dataInput={}
+
+    $scope.onSubmit = function(){
+        console.log($scope.dataInput);
+    }
+
+}
+
+app.controller("myController",myFunction)

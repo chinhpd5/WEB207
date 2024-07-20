9890a87623c955c87ce2cec68785dd442998e3fb
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-20 16:50:58 +0700

    validate

---
+++ lession9/main.js
@@ -0,0 +1,74 @@
+const app = angular.module("myApp",[]);
+
+function myFunction($scope){
+    $scope.listData=[
+        {
+            hobbies: [
+                {
+                    text: "Đá bóng",
+                    name: "football",
+                    value: true
+                },
+                {
+                    text: "Bơi",
+                    name: "swim",
+                    value: false
+                },
+                {
+                    text: "Chạy",
+                    name: "run",
+                    value: true
+                }
+            ],
+            name: "chinhpd5",
+            age: 20,
+            gender: "men",
+            note: "ghi chú"
+        }
+    ]
+
+    $scope.dataInput={
+        hobbies : [
+            {
+                text: "Đá bóng",
+                name: "football",
+                value: false
+            },
+            {
+                text: "Bơi",
+                name: "swim",
+                value: false
+            },
+            {
+                text: "Chạy",
+                name: "run",
+                value: false
+            }
+        ]
+    };
+    $scope.listGender =[
+        {
+            name: "Nam",
+            value: "men"
+        },
+        {
+            name: "Nữ",
+            value: "women"
+        },
+        {
+            name: "Khác",
+            value: "other"
+        }
+    ]
+
+    $scope.onSubmit = function(){
+        // console.log($scope.dataInput);
+        if($scope.frm.$valid){
+            $scope.listData.push(angular.copy($scope.dataInput))
+        }
+
+        console.log($scope.listData);
+    }
+}
+
+app.controller("myController",myFunction)

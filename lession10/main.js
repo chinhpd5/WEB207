0a035babdadcad4d7b036c0eeaba20cd5e358d3c
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-23 18:56:38 +0700

    edit delete

--- lession10/main.js
+++ lession10/main.js
@@ -1,2 +1,2 @@
-// Thực hiện chức năm Danh sách, Thêm, Sửa, Xóa cho Product
+// Thực hiện chức năng Danh sách, Thêm, Sửa, Xóa cho Product
 // Thêm mới: Danh mục sản phẩm(categoryId): sử dụng thẻ select-option
--- lession9/main.js
+++ lession9/main.js
@@ -1,6 +1,7 @@
 const app = angular.module("myApp",[]);
 
 function myFunction($scope,$http){
+    $scope.type ='add';
     $scope.regex ='/^\S+@\S+\.\S+$/';
     $scope.listData=[]
     
@@ -57,22 +58,40 @@ function myFunction($scope,$http){
     }
 
     $scope.onSubmit = function(){
+
+        
         // console.log($scope.dataInput);
         // khi form (tất các input) không có lỗi 
         if($scope.frm.$valid){
-            // $scope.listData.push(angular.copy($scope.dataInput))
-            // thêm mới sử dụng phương post
-            $http.post("http://localhost:3000/students",$scope.dataInput)
-                .then(
-                    // nếu thành công
-                    function success(){
-                        alert("Thêm thành công")
-                    },
-                    // nếu thất bại
-                    function error(){
-                        alert("Thêm thất bại")
-                    }
-                )
+
+            if($scope.type == 'add'){
+                // $scope.listData.push(angular.copy($scope.dataInput))
+                // thêm mới sử dụng phương post
+                $http.post("http://localhost:3000/students",$scope.dataInput)
+                    .then(
+                        // nếu thành công
+                        function success(){
+                            alert("Thêm thành công")
+                        },
+                        // nếu thất bại
+                        function error(){
+                            alert("Thêm thất bại")
+                        }
+                    )
+            }else if( $scope.type == 'edit'){
+                // cập nhật dữ liệu vào db
+                // console.log($scope.dataInput);
+                $http.put(`http://localhost:3000/students/${$scope.dataInput.id}`,$scope.dataInput)
+                    .then(
+                        function success(){
+                            alert("Sửa thành công")
+                        },
+                        function error(){
+                            alert("Sửa thất bại")
+                        }
+                    )
+            }
+            
 
         }
         console.log($scope.listData);
@@ -95,6 +114,42 @@ function myFunction($scope,$http){
 
     }
     getStudent();
+
+    // xóa
+    $scope.onDelete = function(id){
+        // console.log(id);
+
+        if(confirm("Bạn có chắc chắn muốn xóa không?")){
+            $http.delete(`http://localhost:3000/students/${id}`)
+                .then(
+                    function success(){
+                        alert("Xóa thành công")
+                    },function error(){
+                        alert("Xóa thất bại")
+                    }
+                )
+        }
+       
+    }
+
+    // Sửa việc 1
+    $scope.onEdit = function(id){
+        // console.log(id);
+        // thay đổi trạng thái của form
+        $scope.type ='edit';
+        
+        // lấy thông tin student theo id
+        $http.get(`http://localhost:3000/students/${id}`)
+            .then(
+                function success(res){
+                    // console.log(res.data);
+                    $scope.dataInput = res.data
+                },
+                function error(){
+                    alert("Có lỗi")
+                }
+            )
+    }
 
 }
 

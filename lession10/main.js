5071c397ed2121c1ab5fcdaff8fdc1b03c2de7a1
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-23 16:53:20 +0700

    asm 1

--- lession10/main.js
+++ lession10/main.js
@@ -1,4 +1,4 @@
-// Thực hiện chức năm Danh sách, Thêm, Sửa, Xóa cho Product
+// Thực hiện chức năng Danh sách, Thêm, Sửa, Xóa cho Product
 // Thêm mới: Danh mục sản phẩm(categoryId): sử dụng thẻ select-option
 // Yêu cầu validate: 
 /*

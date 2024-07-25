2d5e0032ca14e7f296312f33aac82c83a28306b8
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-25 17:00:31 +0700

    route

--- lession11/main.js
+++ lession11/main.js
@@ -1,1 +1,13 @@
-const app = angular.module("myApp",[])
+const app = angular.module("myApp",['ngRoute'])
+
+app.config(function($routeProvider){
+    // khai báo các đường dẫn
+    $routeProvider
+        .when('/home',{
+            templateUrl: './views/home.html'
+        })
+        .when('/about',{
+            templateUrl: './views/about.html'
+        })
+
+})

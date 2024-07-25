a3c2cd96a9b102fad402b63eb97867cf04357560
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-25 19:42:30 +0700

    liên kết

---
+++ lession11/controllers/ProductController.js
@@ -0,0 +1,3 @@
+window.ProductController = function($scope){
+    
+}
--- lession11/router.js
+++ lession11/router.js
@@ -2,12 +2,17 @@ const app = angular.module("myApp",["ngRoute"])
 
 app.config(function ($routeProvider){
     $routeProvider
-        .when('/home',{
-            templateUrl: './views/home.html',
-            controller: HomeController
-        })
-        .when('/about',{
-            templateUrl: './views/about.html',
-            controller: AboutController
-        })
+    .when('/home',{
+        templateUrl: './views/home.html',
+        controller: HomeController
+    })
+    .when('/about',{
+        templateUrl: './views/about.html',
+        controller: AboutController
+    })
+    .when('/product',{
+        templateUrl: './views/product/list.html',
+        controller: ProductController
+    })
+    
 })

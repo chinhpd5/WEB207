7009ef26426d382256920727c5416a87447a6dbe
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-25 19:56:09 +0700

    list

--- lession11/controllers/ProductController.js
+++ lession11/controllers/ProductController.js
@@ -1,3 +1,36 @@
-window.ProductController = function($scope){
-    
+window.ProductController = function($scope,$http){
+    $scope.listProduct =[]
+    $scope.listCategory =[]
+    // lấy danh sách sản phẩm từ db.json
+    function getProducts(){
+        $http.get('http://localhost:3000/products')
+            .then(
+                function success(res){
+                    // console.log(res.data);
+                    $scope.listProduct = res.data;
+                },
+                function error(){
+                    alert("Lấy danh sách thất bại")
+                }
+            )
+    }
+
+    // lấy danh sách danh mục
+    function getCategories(){
+        $http.get('http://localhost:3000/categories')
+            .then(
+                function success(res){
+                    // console.log(res.data);
+                    $scope.listCategory = res.data;
+                },
+                function error(){
+                    alert("Lấy danh sách thất bại")
+                }
+            )
+    }
+
+    getProducts();
+
+    getCategories();
+
 }

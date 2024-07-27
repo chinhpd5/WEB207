57cc4343e40e348c10384e07747109fbd3437b28
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-27 16:47:04 +0700

    add

--- lession11/controllers/productController.js
+++ lession11/controllers/productController.js
@@ -1,6 +1,7 @@
-window.ProductController = function($scope,$http){
+window.ProductController = function($scope,$http,$location){
     $scope.listProduct =[];
-    $scope.listCategory =[]
+    $scope.listCategory =[];
+    $scope.dataInput={};
 
     // lấy danh sách product
     function getProducts(){
@@ -21,8 +22,12 @@ window.ProductController = function($scope,$http){
         $http.get('http://localhost:3000/categories')
             .then(
                 function success(res){
-                    // console.log(res.data);
-                    $scope.listCategory = res.data
+                    console.log(res.data);
+                    $scope.listCategory = res.data;
+                    // lựa chọn mặc định phần tử đầu tiên trong select option
+                    if($scope.listCategory.length > 0){
+                        $scope.dataInput.categoryId = $scope.listCategory[0].id
+                    }
                 },
                 function error(){
                     alert("Lấy danh sách thất bại")
@@ -34,4 +39,21 @@ window.ProductController = function($scope,$http){
     getProducts()
 
     getCategories()
+
+    $scope.onAdd = function(){
+        if($scope.frm.$valid){
+            // console.log($scope.dataInput);
+            $http.post('http://localhost:3000/products',$scope.dataInput)
+                .then(
+                    function success(){
+                        // alert("Thêm thành công")
+                        // chuyển về trang danh sách sản phẩm
+                        $location.url('/product')
+                    },
+                    function error(){
+                        alert("Thêm thất bại")
+                    }
+                )
+        }
+    }
 }

d99f99838892d3542d18a8e81301e46f40bdd610
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-07-30 16:25:51 +0700

    lab lession13

---
+++ lession13/main.js
@@ -0,0 +1,12 @@
+/**
+ * Yêu cầu:
+ * Thực hiện chức năng Danh sách, Thêm, Sửa, Xóa
+ * Danh sách hiển thị các thông tin 
+ * - lưu ý: Quê quán hiển thị theo tên quê quán
+ * Thêm mới:
+ * Validate: 
+ *  - Họ và tên: Không để trống
+ *  - code: Không để trống, tối thiểu 5 kí tự
+ *  - year (number): Không để trống, giá trị min 1990
+ *  - home: sử dụng select-option , lựa chọn mặc định giá trị cuối cùng
+ */

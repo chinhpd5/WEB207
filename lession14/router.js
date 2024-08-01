433f6c8d111746b351ac469bc6a672f7991a515c
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-08-01 19:36:22 +0700

    bt

---
+++ lession14/router.js
@@ -0,0 +1,6 @@
+/**
+ * validate:
+ * title: không để trống , tối thiểu 3 kí tự
+ * yearOfPublish(number): không để trống, max: 2024
+ * publisher (select-option): lựa chọn mặc định phần tử đầu tiên
+ */
